d9aff705b218cdff1666babe3a9f75a23855734e
Justin Clifford <justoon7.jc@gmail.com>
2020-09-27 03:40:29 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -8,6 +8,7 @@ def count_strings(array)
 end
 
 def count_empty_strings(array)
+  array.count do ||
 
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
